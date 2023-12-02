87653a106f406d5294c82bd630544f59dc090701
John Joven <johnjoven.borromeo@gmail.com>
2023-12-02 11:33:45 +0800

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Our Hotel -  Contact Us</title>
+    <?php require('inc/links.php') ?>
+    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+    <link rel="stylesheet"href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <link rel="stylesheet"href="css/style.css">
+    
+</head>
+<body class="bg-light">
+
+  <?php require('inc/header.php') ?>
+
+  
+
+  <?php require('inc/footer.php') ?>
+
+</body>
+</html>
