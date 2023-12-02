85dc82a1e73f01d47ac99efb49b65aa8fef20ada
maica04 <152460118+maica04@users.noreply.github.com>
2023-12-02 09:50:40 -0800

    Update contact.php
    done contact

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Our Hotel -  Contact Us</title>
-    <?php require('inc/links.php') ?>
+    <?php require('inc/links.php'); ?>
     
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
     <link rel="stylesheet"href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
@@ -13,11 +13,79 @@
 </head>
 <body class="bg-light">
 
-  <?php require('inc/header.php') ?>
+  <?php require('inc/header.php'); ?>
 
-    
+    <div class="my-5px-4">
+      <h2 class="fw-bold h-font text-center">OUR CONTACT</h2>
+      <div class="h-line bg-dark"></div>
+      <p class="text-center mt-3">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
+      </p>
+</div>
+     
+       <div class="container"></div>
+       <div class="col-lg-6 col-md-6 ab-5 px-4"></div>
+       <div class=" bg-white rounded shadow p-4"></div>
+       <iframe class="w-100 rounded mb-4" height="350px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3858.2563839613304!2d121.0309099!3d14.754580400000009!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b1cc9c9c83e9%3A0x303a03298da24ddb!2sUniversity%20of%20Caloocan%20City%20-%20Congressional%20Campus!5e0!3m2!1sen!2sph!4v1701334532767!5m2!1sen!2sph" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+       <h5>Address</h5>
+       <a href="https://maps.app.goo.gl/5mtBB9qgE6hZzP2y5" target="_blank" class=" d-inline-block tex-decoration-non text-dark mb-2"></a>
+       <i class="bi bi-geo-fill"></i>Makati Shangri-La
+      
+         <h5 class="mt-4"> Call us</h5>
+          <a href="#" class="d-inline-block mb-2 text-decoration-none text-dark">09234567890</a>
+       
+        <br>
+          <a href="#" class="d-inline-block mb-2 text-decoration-none text-dark">09154323554</a>
 
-  <?php require('inc/footer.php') ?>
+          <h5 class="mt-4">Email</h5>
+          <a href="mailto:grafiljamaica@gmail.com" class="d-inline-block mb-2 text-decoration-none text-dark">
+          <i class="bi bi-envelope"></i>grafiljamaica@gmail.com
+          </a>
+          <a href="hazeljadelobenaria@gmail.com" class="d-inline-block mb-2 text-decoration-none text-dark">
+          <i class="bi bi-envelope"></i>hazeljadelobenaria@gmail.com
+          </a>
+
+          <h5 class="mt-4">Follow Us</h5>
+          <a href="#" class="d-inline-block text-dark fs-5 "> 
+          <i class="bi bi-instagram"></i>
+          </a>
+         
+          <a href="#" class="d-inline-block  text-dark fs-5 me-2">
+             <i class="bi bi-facebook"> </i>
+            </a>
+  
+          <a href="#" class="d-inline-block  text-dark fs-5 me-2">
+            <i class="bi bi-twitter"></i> 
+          </a>
+      
+
+<div class="container"></div>
+       <div class="col-lg-6 col-md-6 ab-5 px-4"></div>
+    <di class=<div class=" bg-white rounded shadow p-4"></div>
+    <from>
+      <h5>Send a message </h5>
+      <div class="nb-3">
+              <label class="form-label" style= "font-weight: 500">Name</label>
+              <input type="text" class="form-control border border-dark">
+       </div>
+             <div class="nb-3">
+              <label class="form-label" style= "font-weight: 500">Email</label>
+              <input type="email" class="form-control border border-dark">
+            </div>
+            <div class="nb-3">
+              <label class="form-label" style= "font-weight: 500">Subject</label>
+              <input type="text" class="form-control border border-dark">
+       </div>
+       <div class="nb-3">
+              <label class="form-label" style= "font-weight: 500">Message</label>
+              <textarea class="form-control shadow-none border border-dark" rows="5" style="resize: none"></textarea>
+       </div>
+       <button type="submit" class="btn text-white custom-bg mt-3">SEND</button>
+
+
+    </from>
+
+  <?php require('inc/footer.php'); ?>
   <!-- Scripts -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
