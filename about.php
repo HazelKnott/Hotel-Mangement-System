2495d8f3995a886d93a54f62e62e3141c8eb20d5
John Joven <johnjoven.borromeo@gmail.com>
2023-12-02 15:26:32 +0800

    creating about_pg, contact_pg modifying rooms.php

---
+++ about.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Our Hotel -  About Us</title>
+    <?php require('inc/links.php') ?>
+    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+    <link rel="stylesheet"href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <link rel="stylesheet"href="css/style.css">
+    
+</head>
+<body class="bg-light">
+
+  <?php require('inc/header.php') ?>
+
+    
+
+  <?php require('inc/footer.php') ?>
+  <!-- Scripts -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
--- rooms.php
+++ rooms.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Our Hotel -  Contact Us</title>
+    <title>Our Hotel - Our Rooms</title>
     <?php require('inc/links.php') ?>
     
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
