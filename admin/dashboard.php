c741eb7779c91d786f5058cf19c774808871a3d7
John Joven <johnjoven.borromeo@gmail.com>
2023-12-04 14:49:09 +0800

    admin dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -7,20 +7,24 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE-edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Panel - Dashboard</title>
-    <?php require('inc/links.php') ?>
+    <?php require('inc/links.php'); ?>
 </head>
+
 <body class="bg-white">
     
-    <div class="container-fluid bg-dark text-light p-3 d-flex align-items-center justify-content-between">
-        <h3 class="mb-0">ADMIN PANEL</h3>
-        <a href="logout.php" class="btn btn-light btn-sm">Log-out</a>
-    </div>
+    <?php require('inc/header.php'); ?>
 
-
-
-
-<?php require('inc/scripts.php') ?>
+		<div class="container-fluid" id="main-content">
+			<div class="row">
+				<div class="col-lg-10 ms-auto p-4 overflow-hidden">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae expedita aspernatur nesciunt. Consectetur voluptates esse vitae iure labore cum natus rem quo assumenda nemo, voluptate explicabo adipisci provident fuga placeat vero delectus optio alias repudiandae dignissimos quas voluptatibus! Molestiae fugit laborum cum incidunt, eveniet sit quaerat unde esse eos nam fuga impedit in omnis modi mollitia ea nobis debitis, quas minus, magni veritatis accusantium officia quo! Necessitatibus iste voluptatem, nam repellat quos illo qui dolores sunt libero aperiam minima! Iure rerum reiciendis cum ullam. Consequuntur alias quod corrupti voluptates reprehenderit voluptate ipsum eaque quos! Fuga soluta iusto alias perspiciatis. Et provident deleniti est fuga iure perferendis voluptatum saepe beatae soluta quas atque temporibus ut, consequatur autem quos veniam? Excepturi ut rem reprehenderit possimus, molestiae error similique non magni, nisi natus ratione exercitationem, eos corporis obcaecati dignissimos dicta voluptate totam alias? Neque nulla commodi illo, non magni voluptatibus minus doloribus itaque ipsum veniam aut maxime quos, aperiam consectetur quisquam nemo esse odio ducimus vitae facilis ipsam! Asperiores nesciunt quaerat qui beatae ad consequatur, quasi aut maiores possimus iste, excepturi non sed voluptas reiciendis, ex optio at quibusdam similique perspiciatis cum atque. Fuga totam, consectetur illo repellat a odio distinctio numquam recusandae.
+				</div>
+			</div>
+		</div>
+		
+<?php require('inc/scripts.php'); ?>
 </body>
 </html>
---
+++ admin/inc/header.php
@@ -0,0 +1,31 @@
+<div class="container-fluid bg-dark text-light p-3 d-flex align-items-center justify-content-between sticky-top">
+        <h3 class="mb-0">Tranquil Hotel</h3>
+        <a href="logout.php" class="btn btn-light btn-sm">Log-out</a>
+    </div>
+
+    <div class="col-lg-2 bg-dark border-top border-3 border-secondary" id="dashboard-menu">
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <div class="container-fluid flex-lg-column align-items-stretch">
+                <h4 class="mt-2 text-light">ADMIN PANEL</h4>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#adminDropdown" aria-controls="adminDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="adminDropdown">
+                    <ul class="nav nav-pills flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="dashboard.php">Dashboard</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="#">Rooms</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="#">Users</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="#">Settings</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
